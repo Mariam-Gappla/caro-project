52d67c20e7be3a9e14194971699ce518b5868021
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-07-31 12:11:04 +0200

    fix error in contact us

--- validation/contactUsValidition.js
+++ validation/contactUsValidition.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 const getMessages = require('../locales/schemaValiditionMessages/contactUsValiditionMessages');
 
 const contactUsSchema = (lang = 'en') => {
-  const messages = getMessages(lang);
+  const m = getMessages(lang);
 
     return Joi.object({
     name: Joi.string().required().messages({
@@ -16,15 +16,6 @@ const contactUsSchema = (lang = 'en') => {
     message: Joi.string().required().messages({
       'any.required': m.message,
       'string.empty': m.message
-    }),
-    senderType: Joi.string().valid('user', 'serviceProvider', 'rentalOffice').required().messages({
-      'any.required': m.senderType,
-      'any.only': m.senderType,
-      'string.empty': m.senderType
-    }),
-    senderId: Joi.string().required().messages({
-      'any.required': m.senderId,
-      'string.empty': m.senderId
     })
   });
 
