52a869470e7683eb2aa52bfc62626153da81eca6
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-08-09 16:41:45 +0200

    fix error

--- routes/rentalOfficeroutes.js
+++ routes/rentalOfficeroutes.js
@@ -1,10 +1,11 @@
 const express=require("express");
 const router=express.Router();
-const {getAllRentallOffice,addLike,getRentalOfficeCar,getProfileData}=require("../controllers/rentalOffice");
+const {getAllRentallOffice,addLike,getRentalOfficeCar,getProfileData,editRentalOfficeProfile}=require("../controllers/rentalOffice");
 router.get("/all",getAllRentallOffice);
 router.get("/overview",getRentalOfficeCar);
 router.get("/profileData",getProfileData)
 router.patch("/Like/:id",addLike);
+router.put("/rentalOffice",editRentalOfficeProfile)
 
 
 
