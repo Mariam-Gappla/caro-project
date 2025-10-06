dba6b8ae5a6df3e82d7a6d297c5b82566b506cd1
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-10-06 19:04:14 +0200

    make rating and rating

--- models/report.js
+++ models/report.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const reportSchema = new mongoose.Schema({
   userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },   // مين اللي بلغ
   entityId: { type: mongoose.Types.ObjectId, required: true },              // البوست أو الكومنت
-  entityType:{ type: String, enum: ["Post","ShowRoomPosts"], required: true },
+  entityType:{ type: String, enum: ["Post","ShowRoomPosts","Car","CarPlate"], required: true },
   isViolation:{type:Boolean,default:false},                           // هل تم التأكد من وجود مخالفة
   reason: { type: String, required: true },                                 // سبب الإبلاغ
   status: { type: String, enum: ["Pending", "Reviewed", "Rejected"], default: "Pending" }
