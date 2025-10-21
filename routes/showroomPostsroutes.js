05f6ba4224b6a1e611804bf22d26490370983a5c
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-10-21 10:26:53 +0200

    fix error in show room

--- routes/showroomPostsroutes.js
+++ routes/showroomPostsroutes.js
@@ -6,7 +6,7 @@ router.post("/",upload.fields([
     { name:"images"},
     { name:"video"}
 ]), addShowroomPost);
-router.get("/",getShowroomPosts)
+router.get("/posts:showroomId",getShowroomPosts)
 router.get("/:id",getPostById)
 
 
