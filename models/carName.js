8ede1f2c6d28eefd98bbd4c7e0120867087c424f
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-11-01 11:24:31 +0200

    make edits

--- models/carName.js
+++ models/carName.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
 const carNamerSchema = new mongoose.Schema({
-  rentalOfficeId: { type: mongoose.Schema.Types.ObjectId, ref: 'rentalOffice', required: true },
   image:{type: String, required: true},
   carName: {
     en: { type: String, required: true },
