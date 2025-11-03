9e83b535b8c3ae15331b36ac96e7ef79cab6186a
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-11-03 14:40:04 +0200

    make edits

--- controllers/rentalOfficeOrders.js
+++ controllers/rentalOfficeOrders.js
@@ -1121,7 +1121,10 @@ const getAllUserOrders = async (req, res, next) => {
                     paymentStatus: order.paymentStatus,
                     paymentStatusText,
                     createdAt: order.createdAt,
-                    userData:providerOrders.providerId
+                    userData:{
+                        username:order.providerId.username,
+                        image:order.providerId.image
+                    }
                 };
             })
         );
