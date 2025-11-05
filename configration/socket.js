71cd970187d376b4eb5bde8225b0e180136d7d97
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-11-05 15:42:47 +0200

    get slavge post by id

--- configration/socket.js
+++ configration/socket.js
@@ -1,13 +1,9 @@
 const { handleMessage } = require("../controllers/chat");
-const { placeBid } = require("../controllers/auctionOrder");
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("🔌 مستخدم اتصل:", socket.id);
-
     // استدعاء دوال بتتعامل مع أنواع مختلفة من الأحداث
     handleMessage(io, socket);
-    placeBid(io, socket);
-
     // عند فصل الاتصال
     socket.on("disconnect", () => {
       console.log("❌ المستخدم خرج:", socket.id);
