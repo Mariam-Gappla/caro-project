078f9fd730792f66a49bce3a0414a9868eef6897
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-11-05 15:15:36 +0200

    get slavge post by id

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const path = require("path");
 const { Server } = require("socket.io");
 const server = http.createServer(app);
