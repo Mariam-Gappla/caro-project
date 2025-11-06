a2895c09f3f487ea12305260ec2f381811bfbea0
Mariam-Gappla <119626394+Mariam-Gappla@users.noreply.github.com>
2025-11-06 04:28:45 +0200

    fix error in auction order

--- routes/serviceProviderPricingroutes.js
+++ routes/serviceProviderPricingroutes.js
@@ -1,8 +1,8 @@
 const express=require("express");
 const router=express.Router();
 const {addBatteryPricing,addTirePricing,addWinchPricing,getPricing}=require("../controllers/serviceProviderPrice");
+router.get("/",getPricing)
 router.post("/winch-pricing",addWinchPricing);
 router.post("/tire-pricing",addTirePricing);
 router.post("/battery-pricing",addBatteryPricing);
-router.get("/",getPricing)
 module.exports=router;
